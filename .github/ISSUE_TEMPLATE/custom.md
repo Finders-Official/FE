6e8d9b7d6de1316d9b5250df0188b148c2ee7b9f
jeonbinggu <tinyjbk123@gmail.com>
2025-12-24 03:10:02 +0900

    chore: 프로젝트 초기 세팅 (#1)

--- .github/ISSUE_TEMPLATE/custom.md
+++ .github/ISSUE_TEMPLATE/custom.md
@@ -2,19 +2,20 @@
 name: Custom issue template
 about: 해당 이슈 생성 템플릿을 사용해 주세요
 title: "[FEAT/BUG/REFACTOR/CHORE] 이슈 이름"
-labels: ''
-assignees: ''
-
+labels: ""
+assignees: ""
 ---
 
 ## 📑 Description
+
 <!--추가/수정이 필요한 내용-->
 
-
 ## 📌 To Do
+
 <!--추가/수정될 내용-->
-- [ ] 
-- [ ] 
-- [ ] 
+
+- [ ]
+- [ ]
+- [ ]
 
 ## 💬 Comment
--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import { defineConfig, globalIgnores } from 'eslint/config';
-import prettierConfig from 'eslint-config-prettier';
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettierConfig from "eslint-config-prettier";
 
 export default defineConfig([
-  globalIgnores(['dist', 'build', 'coverage', 'node_modules']),
+  globalIgnores(["dist", "build", "coverage", "node_modules"]),
 
   {
-    files: ['**/*.{js,jsx,ts,tsx}'],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
