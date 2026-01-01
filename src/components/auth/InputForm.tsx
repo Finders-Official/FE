2fe9fc0c4abc492438fcedd5f960c9cca4bed06c
jeonbinggu <tinyjbk123@gmail.com>
2026-01-01 21:17:32 +0900

    feat: Inputform 컴포넌트 UI 구현(#5)

--- src/components/auth/InputForm.tsx
+++ src/components/auth/InputForm.tsx
@@ -1,3 +1,32 @@
-export const InputForm = () => {
-  return <div>Input Form Component</div>;
+type InputFormSize = "medium" | "large";
+
+interface InputFormProps {
+  name: string;
+  placeholder: string;
+  size: InputFormSize;
+  className?: string;
+}
+
+export const InputForm = ({
+  name,
+  placeholder,
+  size,
+  className,
+}: InputFormProps) => {
+  const sizeClass: Record<InputFormSize, string> = {
+    medium: "h-[3.25rem] w-[15.75rem]",
+    large: "h-[3.25rem] w-full",
+  };
+  return (
+    <div>
+      <p>{name}</p>
+      <form className="mt-[1rem]">
+        <input
+          placeholder={placeholder}
+          name={name}
+          className={`${sizeClass[size]} ${className} bg-neutral-875 rounded-lg p-2 placeholder:text-neutral-600 focus:outline-none`}
+        ></input>
+      </form>
+    </div>
+  );
 };
