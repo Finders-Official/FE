b9896d2c0de561964c73ddc1ed0942949f25623d
jeonbinggu <tinyjbk123@gmail.com>
2026-01-01 20:01:57 +0900

    chore: 이미지 파일 assets/icon 폴더로 이동 및 경로 수정(#5)

--- src/components/auth/AppleButton.tsx
+++ src/components/auth/AppleButton.tsx
@@ -1,3 +1,5 @@
+import AppleLogo from "../../assets/icon/Apple.svg";
+
 interface AppleButtonProps {
   onClick?: () => void;
 }
@@ -8,7 +10,7 @@ export const AppleButton = ({ onClick }: AppleButtonProps) => {
       className="bg-neutral-875 inline-flex h-[3.125rem] w-full items-center justify-center gap-2 rounded-2xl font-semibold text-white shadow-sm active:scale-[0.99]"
       onClick={onClick}
     >
-      <img src="../public/Apple.svg" alt="Apple Logo" className="h-6 w-6" />
+      <img src={AppleLogo} alt="Apple Logo" className="h-6 w-6" />
       <span>애플로 로그인</span>
     </button>
   );
--- src/components/auth/KakaoButton.tsx
+++ src/components/auth/KakaoButton.tsx
@@ -1,3 +1,5 @@
+import KakaoLogo from "../../assets/icon/Kakao.svg";
+
 interface KakaoButtonProps {
   onClick?: () => void;
 }
@@ -8,7 +10,7 @@ const KakaoButton = ({ onClick }: KakaoButtonProps) => {
       className="inline-flex h-[3.125rem] w-full items-center justify-center gap-2 rounded-2xl bg-[#FEE500] font-semibold text-neutral-900 shadow-sm active:scale-[0.99]"
       onClick={onClick}
     >
-      <img src="../public/Kakao.svg" alt="Kakao Logo" className="h-6 w-6" />
+      <img src={KakaoLogo} alt="Kakao Logo" className="h-6 w-6" />
       <span>카카오 로그인</span>
     </button>
   );
