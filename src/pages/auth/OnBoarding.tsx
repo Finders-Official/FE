98b7bd11a1fd15b510b3c0de703c43c8baf77551
jeonbinggu <tinyjbk123@gmail.com>
2026-01-06 00:19:18 +0900

    feat: 전화번호 인증 로직 및 UI 1차 구현(#15)

--- src/pages/auth/OnBoarding.tsx
+++ src/pages/auth/OnBoarding.tsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import { InputForm } from "../../components/auth/InputForm";
 
 export default function OnBoardingPage() {
+  const [isVerifyed, setIsVerified] = useState(false);
+  const [phone, SetPhone] = useState("");
+
+  const handleVerify = () => {
+    // 인증 로직 구현
+    setIsVerified(true);
+  };
+
+  const buttonBgClass = phone ? "bg-orange-500" : "bg-neutral-875";
   return (
     <div className="mt-[3.4375rem] w-full">
       <header className="pt-[2.5rem]">
@@ -21,9 +31,19 @@ export default function OnBoardingPage() {
             placeholder="'-'제외하고 입력"
             size="medium"
             className="focus:border-orange-500"
+            value={phone}
+            onChange={(e) => {
+              const onlyDigits = e.target.value.replace(/\D/g, "").slice(0, 11);
+              SetPhone(onlyDigits);
+            }}
           />
-          <button className="bg-neutral-875 mt-auto h-[3.25rem] w-[5.25rem] rounded-lg active:scale-[0.99] active:bg-orange-500">
-            인증하기
+          {/* 인증하기 버튼 누르고 발송되면 재발송으로 코멘트 바꾸기 */}
+          <button
+            type="button"
+            className={`mt-auto h-[3.25rem] w-[5.25rem] rounded-lg active:scale-[0.99] active:bg-orange-500 ${buttonBgClass}`}
+            onClick={handleVerify}
+          >
+            {isVerifyed ? "재발송" : "인증하기"}
           </button>
         </section>
       </main>
@@ -34,3 +54,12 @@ export default function OnBoardingPage() {
 // 닉네임 중복 여부 -> 서버에서 소통 -> useDebounce나 usethrottle로 일정 시간 뒤에 검사
 // 닉네임 형식 불일치 여부
 // 전화번호 인증 방식 (나중에)
+
+//닉네임: 이미 존재하는 닉네임입니다. -> 닉네임 존재 여부 api
+// 2~8자 이내로 입력해주세요 / 사용가능한 아이디입니다.
+
+//전화번호 인증
+//1. 인증번호 발송 api
+//2. 인증번호 확인 api
+
+//모두 완료 후 회원가입 완료 상태 넘기고 라우팅 설정
