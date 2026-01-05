e4d5fb23a1cf3358f502bb324330eeca97166b00
jeonbinggu <tinyjbk123@gmail.com>
2026-01-06 00:18:36 +0900

    feat: InputForm 컴포넌트 props 추가(#15)

--- src/components/auth/InputForm.tsx
+++ src/components/auth/InputForm.tsx
@@ -5,6 +5,8 @@ interface InputFormProps {
   placeholder: string;
   size: InputFormSize;
   className?: string;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const InputForm = ({
@@ -12,6 +14,8 @@ export const InputForm = ({
   placeholder,
   size,
   className,
+  value,
+  onChange,
 }: InputFormProps) => {
   const sizeClass: Record<InputFormSize, string> = {
     medium: "h-[3.25rem] w-[15.75rem]",
@@ -25,6 +29,8 @@ export const InputForm = ({
           id={name}
           placeholder={placeholder}
           name={name}
+          value={value}
+          onChange={onChange}
           className={`${sizeClass[size]} ${className} rounded-lg border border-neutral-800 p-2 placeholder:text-neutral-600 focus:outline-none`}
         ></input>
       </form>
