666590ef7a958a2b09273b8696020b75f92989d6
jeonbinggu <tinyjbk123@gmail.com>
2026-01-08 21:07:57 +0900

    feat: tab 타입 정의(#26)

---
+++ src/types/tab.ts
@@ -0,0 +1,13 @@
+import React from "react";
+
+export type IconComponent = React.ComponentType<React.SVGProps<SVGSVGElement>>;
+
+export type TabItem = {
+  to: string;
+  label: string;
+  icon: IconComponent;
+  activeicon: IconComponent;
+  end?: boolean;
+};
+
+//tabbar 카테고리에 사용되는 type을 지정
