631f5c2d44ef21733dbd25b646d12f9294dba53e
jeonbinggu <tinyjbk123@gmail.com>
2026-01-08 16:37:27 +0900

    feat: TabBar UI 2차 구현(#26)

--- src/components/common/TabBar.tsx
+++ src/components/common/TabBar.tsx
@@ -1,17 +1,61 @@
+import { KakaoIcon } from "@/assets/icon";
+
 export const TabBar = () => {
   return (
-    <div className="fixed bottom-0 left-1/2 z-50 mx-auto h-[5.5625rem] w-full -translate-x-1/2 bg-neutral-900 p-[1.25rem]">
-      {/*1.홈버튼
-      2. 현상소 보기 버튼
-      3.사진 수다 버튼
-      4.현상 관리 버튼
-      5. 마이페이지 버튼*/}
-      <nav className="grid grid-cols-5">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
+    <div className="fixed bottom-0 left-1/2 z-50 h-[5.5625rem] w-full max-w-6xl -translate-x-1/2 bg-neutral-900 px-[1.5rem] py-[1.25rem]">
+      {/*
+      1.홈버튼 -> /mainpage
+      2. 현상소 보기 버튼 -> photolab-list
+      3.사진 수다 버튼 -> /photofeed 
+      4.현상 관리 버튼 -> development-manage
+      5. 마이페이지 버튼 -> /mypage
+      */}
+      <nav className="grid h-full grid-cols-5 gap-1">
+        <button
+          type="button"
+          className="flex flex-col items-center justify-center"
+        >
+          <KakaoIcon className="h-[1.5rem] w-[1.5rem]" fill="orange-500" />
+          <label className="mt-auto text-center text-xs text-neutral-300">
+            홈
+          </label>
+        </button>
+        <button
+          type="button"
+          className="flex flex-col items-center justify-center"
+        >
+          <KakaoIcon className="h-[1.5rem] w-[1.5rem]" />
+          <label className="mt-auto text-center text-xs text-neutral-300">
+            현상소 보기
+          </label>
+        </button>
+        <button
+          type="button"
+          className="flex flex-col items-center justify-center"
+        >
+          <KakaoIcon className="h-[1.5rem] w-[1.5rem]" />
+          <label className="mt-auto text-center text-xs text-neutral-300">
+            사진수다
+          </label>
+        </button>
+        <button
+          type="button"
+          className="flex flex-col items-center justify-center"
+        >
+          <KakaoIcon className="h-[1.5rem] w-[1.5rem]" />
+          <label className="mt-auto text-center text-xs text-neutral-300">
+            현상 관리
+          </label>
+        </button>
+        <button
+          type="button"
+          className="flex flex-col items-center justify-center"
+        >
+          <KakaoIcon className="h-[1.5rem] w-[1.5rem]" />
+          <label className="mt-auto text-center text-xs text-neutral-300">
+            마이페이지
+          </label>
+        </button>
       </nav>
     </div>
   );
