9a6ff2ed88990d5929d642332a0ff30e64b14103
jeonbinggu <tinyjbk123@gmail.com>
2026-01-09 19:25:27 +0900

    feat: 마이페이지탭 컴포넌트 구현(#31)

---
+++ src/constants/mypage/mypagetab.constant.ts
@@ -0,0 +1,8 @@
+import { LikedPhotoLabIcon, LikedPostIcon, MyPostIcon } from "@/assets/icon";
+import type { TabItem } from "@/types/mypage/mypagetab";
+
+export const tabs: TabItem[] = [
+  { to: "/liked-photolabs", label: "관심 현상소", Icon: LikedPhotoLabIcon },
+  { to: "/liked-posts", label: "관심 게시글", Icon: LikedPostIcon },
+  { to: "/my-posts", label: "내가 쓴 글", Icon: MyPostIcon },
+];
