9ec1ef1893a3393801f58e970b95493a50c722d8
jeonbinggu <tinyjbk123@gmail.com>
2026-01-09 21:52:50 +0900

    refactor: 마이페이지 컴포넌트 분리(#31)

--- src/components/mypage/OptionLink.tsx
+++ src/components/mypage/OptionLink.tsx
@@ -1,22 +1,19 @@
 import { ChevronLeftIcon } from "@/assets/icon";
-import type { IconComponent } from "@/types/icon";
+import type { OptionLinkItem } from "@/types/mypage/optionlink";
 
 import { NavLink } from "react-router";
 
-type OptionLinkProps = {
-  to: string;
-  text: string;
-  Icon?: IconComponent;
-};
-
-export const OptionLink = ({ to, text, Icon }: OptionLinkProps) => {
+export const OptionLink = ({ to, text, info, Icon }: OptionLinkItem) => {
   return (
     <NavLink
       to={to}
       className="flex h-[4rem] w-full items-center gap-3 px-[1rem]"
     >
       {Icon && <Icon className="h-[1rem] w-[1.125rem]" />}
-      <p className="flex-1">{text}</p>
+      <div className="flex flex-1 justify-between">
+        <p>{text}</p>
+        {info ? <p>{info}</p> : null}
+      </div>
       <ChevronLeftIcon className="h-[1.25rem] w-[1.25rem] rotate-180" />
     </NavLink>
   );
--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,9 +1,15 @@
-import { ChevronLeftIcon, TicketFillIcon } from "@/assets/icon";
+import { ChevronLeftIcon } from "@/assets/icon";
 import { MyPageTabs } from "@/components/mypage/MyPageTab";
 import { OptionLink } from "@/components/mypage/OptionLink";
+import { info } from "@/constants/mypage/info.constant";
+import {
+  managelist,
+  servielist,
+} from "@/constants/mypage/servicelist.constant";
 import { NavLink } from "react-router";
 
 export function MyPage() {
+  const { member, roleData } = info;
   return (
     <div className="w-full pt-[5rem]">
       <header className="border-neutral-875 border-b-[0.4rem] p-[1rem]">
@@ -12,8 +18,8 @@ export function MyPage() {
           <div className="border-radius-100 h-[3.75rem] w-[3.75rem] rounded-full border border-neutral-800"></div>
           {/* 이름 / 닉네임 */}
           <div className="flex flex-1 flex-col">
-            <p className="font-light">김필름</p>
-            <p className="font-normal">kimmmmmp</p>
+            <p className="font-light">{member.name}</p>
+            <p className="font-normal">{roleData.user?.nickname}</p>
           </div>
           {/*이동 아이콘*/}
           <NavLink to="./edit-info">
@@ -24,9 +30,24 @@ export function MyPage() {
       </header>
       <main>
         <section className="border-neutral-875 border-b">
-          <OptionLink Icon={TicketFillIcon} text="티켓 충전" to="/" />
+          {managelist.map((item) => (
+            <OptionLink
+              key={item.text}
+              {...item}
+              info={
+                item.text === "티켓 충전"
+                  ? `${roleData.user?.tokenBalance ?? 0}장`
+                  : undefined
+              }
+            />
+          ))}
         </section>
-        <section></section>
+        <section>
+          <p className="p-[1rem]">고객 지원</p>
+          {servielist.map(({ to, text, Icon }) => (
+            <OptionLink to={to} text={text} Icon={Icon} />
+          ))}
+        </section>
       </main>
     </div>
   );
