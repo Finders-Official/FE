26f8a0167d12abd5cc28579efd46afe9568d612c
hyunseo-han <ols3040015@naver.com>
2026-01-11 05:02:10 +0900

    feat: 헤더 구현(#9)

--- src/components/mainPage/Header.tsx
+++ src/components/mainPage/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 flex h-14 w-full items-center justify-between bg-black/80 px-5 backdrop-blur-md">
+    <header className="sticky top-0 z-50 flex h-14 w-full items-center justify-between bg-neutral-900 px-5">
       <div>
         <Link
           to="/"
