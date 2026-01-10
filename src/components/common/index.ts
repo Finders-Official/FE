e4da3d5a20990c610de1fd67d4ff37407d3c690a
MinSeokKim <kimjunseok23@gmail.com>
2026-01-10 23:02:32 +0900

    fix: conflict없이 발생한 conflict 해결

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -13,16 +13,6 @@ export type { SearchItemProps, SearchItemType } from "./SearchItem";
 export { CTA_Button } from "./CTA_Button";
 export { ImageCard } from "./ImageCard";
 export { TextArea } from "./TextArea";
-export { default as Header } from "./Header";
-export type { HeaderProps, RightAction } from "./Header";
-export { default as Icon } from "./Icon";
-export type { IconProps, IconSize } from "./Icon";
-
-export { default as SearchBar } from "./SearchBar";
-export type { SearchBarProps, RightIconType } from "./SearchBar";
-
-export { default as SearchItem } from "./SearchItem";
-export type { SearchItemProps, SearchItemType } from "./SearchItem";
 
 export { default as UnderlineTabs } from "./UnderlineTabs";
 export type { UnderlineTabsProps, TabItem } from "./UnderlineTabs";
