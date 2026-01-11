e952587f46a45f7a38816fb2890717df377becba
hyunseo-han <ols3040015@naver.com>
2026-01-11 11:56:05 +0900

    feat: 인기 현상소 구현(#9)

---
+++ src/components/mainPage/PopularLabsSection/PopularLabCard.tsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router";
+
+export interface Lab {
+  id: number;
+  name: string;
+  thumbnail: string;
+  workCount: number;
+  tags: string[];
+}
+
+interface PopularLabCardProps {
+  lab: Lab;
+}
+
+export default function PopularLabCard({ lab }: PopularLabCardProps) {
+  return (
+    <Link
+      to={`/lab/${lab.id}`}
+      className="relative block aspect-163/230 w-full overflow-hidden rounded-[10px] border border-neutral-800"
+    >
+      <img
+        src={lab.thumbnail}
+        alt={lab.name}
+        className="absolute inset-0 h-full w-full object-cover"
+      />
+
+      <div
+        className="absolute right-0 bottom-16.25 left-0 z-10 h-13.25 w-full"
+        style={{
+          background:
+            "linear-gradient(180deg, rgba(22, 22, 22, 0) 0%, rgba(20, 20, 20, 0.677885) 28.37%, #131313 100%)",
+        }}
+      />
+
+      {/* 태그 */}
+      <div className="absolute bottom-18.75 left-2.5 z-20 flex gap-1.5">
+        {lab.tags.map((tag, index) => (
+          <span
+            key={index}
+            className="flex items-center justify-center rounded-lg border border-neutral-800 px-2 py-1.5 text-[10px] leading-[126%] font-semibold tracking-[-0.02em] text-neutral-200"
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+
+      <div className="absolute right-0 bottom-0 left-0 z-20 flex h-16.25 flex-col justify-start gap-0.5 rounded-b-[10px] bg-neutral-900 px-2.5 py-3.5">
+        {/* 상호명 */}
+        <h3 className="w-full text-[14px] leading-[140%] font-semibold tracking-[-0.02em] text-neutral-100">
+          {lab.name}
+        </h3>
+
+        {/* 작업 건수 */}
+        <div className="flex items-center gap-1">
+          <span className="font-regular text-[12px] leading-[126%] tracking-[-0.02em] text-neutral-200">
+            총 작업 건 수
+          </span>
+          <span className="font-regular text-[12px] leading-[126%] tracking-[-0.02em] text-orange-500">
+            {lab.workCount}건
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+// aspect-163/230
+// background 다시 확인하기
