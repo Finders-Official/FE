ee2465ff763a60202e3e209471203ce0963c0406
MinSeokKim <kimjunseok23@gmail.com>
2026-01-13 04:24:18 +0900

    feat: FilterTagList 컴포넌트 추가

--- src/components/photoLab/index.ts
+++ src/components/photoLab/index.ts
@@ -2,3 +2,4 @@ export { default as TagBadge } from "./TagBadge";
 export { default as LabNewsBanner } from "./LabNewsBanner";
 export { default as LabCard } from "./LabCard";
 export { default as LabList } from "./LabList";
+export { default as FilterTagList } from "./FilterTagList";
