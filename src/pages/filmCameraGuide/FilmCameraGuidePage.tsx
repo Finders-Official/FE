592aadd7a2d82624254844b20e2cfe66f4d74129
hyunseo-han <ols3040015@naver.com>
2026-01-13 10:10:37 +0900

    feat: 필카 입문 페이지 HM-030 구현(#49)

---
+++ src/pages/filmCameraGuide/FilmCameraGuidePage.tsx
@@ -0,0 +1,57 @@
+import { useNavigate } from "react-router";
+import { FILM_CAMERA_GUIDE_DATA } from "./constants";
+import { GuideContentCard } from "@/components/filmCameraGuide/GuideContentCard";
+import { ChevronLeftIcon } from "@/assets/icon";
+
+const FilmCameraGuidePage = () => {
+  const navigate = useNavigate();
+
+  const handleCardClick = (id: number) => {
+    navigate(`/film-camera-guide/${id}`);
+  };
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className="mx-auto min-h-screen w-full max-w-md bg-neutral-900 pb-10 text-neutral-100">
+      {/* 메인 헤더 (Navigation Bar) */}
+      <header className="sticky top-0 z-20 flex h-15.25 w-full items-center gap-5 bg-neutral-900 py-4.5">
+        <button
+          onClick={handleBack}
+          className="-ml-1 flex items-center justify-center"
+          aria-label="뒤로가기"
+        >
+          <ChevronLeftIcon className="neutral-200 h-6 w-6" />
+        </button>
+        <h1 className="text-[16px] leading-[155%] font-semibold tracking-[-0.02em] text-neutral-100">
+          필카 입문 101
+        </h1>
+      </header>
+
+      {/* 서브 헤더 */}
+      <section className="sticky top-15 z-10 bg-neutral-900 pt-2 pb-5">
+        <h2 className="mb-1.5 text-[20px] leading-[1.3] font-semibold tracking-[-0.02] text-neutral-100">
+          필름 카메라, 처음이라면 여기부터
+        </h2>
+        <p className="text-[16px] leading-normal font-normal text-neutral-100">
+          필름 초심자를 위한 콘텐츠를 다 모아
+        </p>
+      </section>
+
+      {/* 히어로 이미지 리스트 */}
+      <section className="flex flex-col gap-5">
+        {FILM_CAMERA_GUIDE_DATA.map((data) => (
+          <GuideContentCard
+            key={data.id}
+            content={data}
+            onClick={handleCardClick}
+          />
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default FilmCameraGuidePage;
