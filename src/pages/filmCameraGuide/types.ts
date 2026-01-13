9dbea2073a6cd2fe8c625c43f62d3fd5839c01f6
hyunseo-han <ols3040015@naver.com>
2026-01-13 10:44:59 +0900

    fix: 타입 수정(#49)

--- src/pages/filmCameraGuide/types.ts
+++ src/pages/filmCameraGuide/types.ts
@@ -1,18 +1,21 @@
 export interface GuideContentSection {
-  heading: string; // 소제목
-  subHeading?: string; // 부가 정보
-  imageUrl: string; // 섹션별 이미지
-  description: string; // 본문 설명
+  // 주황색 타이틀용, ID 2,3
+  heading?: string;
+
+  // 위치 텍스트용, ID 1
+  locationHeading?: string;
+
+  subHeading?: string;
+  imageUrl: string;
+  description: string;
 }
 
 export interface FilmCameraGuide {
   id: number;
-  title: string; // 카드 및 헤더 제목
-  summary: string; // 카드 요약문
-  thumbnailUrl: string; // 리스트 썸네일 및 상세 페이지 헤더 이미지
-
-  // 상세 페이지용
-  introText?: string; // 본격적인 리스트 시작 전 서론
-  toc?: string[]; // 목차 리스트
-  contents?: GuideContentSection[]; // 본문 섹션 배열
+  title: string;
+  summary: string;
+  thumbnailUrl: string;
+  toc?: string[];
+  introText?: string;
+  contents?: GuideContentSection[];
 }
