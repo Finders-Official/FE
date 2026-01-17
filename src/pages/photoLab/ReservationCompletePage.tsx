b685d840d3e54ebd10cd616e3f89b43efd304329
MinSeokKim <kimjunseok23@gmail.com>
2026-01-17 13:21:59 +0900

    feat: 예약 완료 메시지 추가

--- src/pages/photoLab/ReservationCompletePage.tsx
+++ src/pages/photoLab/ReservationCompletePage.tsx
@@ -1,6 +1,6 @@
 import { useNavigate, useLocation, useParams } from "react-router";
 import { Header } from "@/components/common";
-import { XMarkIcon } from "@/assets/icon";
+import { XMarkIcon, ConfirmationIcon } from "@/assets/icon";
 
 interface LocationState {
   labName?: string;
@@ -29,7 +29,21 @@ export default function ReservationCompletePage() {
         }}
       />
 
-      <main className="pb-32"></main>
+      <main className="pb-32">
+        {/* 예약 성공 메시지 */}
+        <section className="border-neutral-850 -mx-4 flex flex-col items-center gap-[1.375rem] border-b-[6px] px-4 py-[1.875rem]">
+          <ConfirmationIcon className="h-[3rem] w-[3rem]" />
+          <div className="flex flex-col items-center gap-0.5">
+            <h1 className="text-[1.125rem] leading-[155%] font-semibold tracking-[-0.02em]">
+              <span className="text-orange-500">현상소 예약</span>
+              <span className="text-neutral-100">이 완료되었어요</span>
+            </h1>
+            <p className="text-[0.875rem] leading-[155%] font-normal tracking-[-0.02em] text-neutral-500">
+              정성을 담아 소중한 추억을 현상해드릴게요
+            </p>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
