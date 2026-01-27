eee00f926e7032c539b5c82e5002c6d01ec86416
MinSeokKim <kimjunseok23@gmail.com>
2026-01-28 04:06:51 +0900

    feat: 현상소 favorite용 훅 생성

--- src/hooks/photoLab/index.ts
+++ src/hooks/photoLab/index.ts
@@ -1,3 +1,4 @@
 export { useGeolocation } from "./useGeolocation";
 export { useIntersectionObserver } from "./useIntersectionObserver";
 export { usePhotoLabList } from "./usePhotoLabList";
+export { useFavoriteToggle } from "./useFavoriteToggle";
