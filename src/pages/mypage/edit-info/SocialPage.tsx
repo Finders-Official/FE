eab59e174b30d81ae6a7fc29c5b568cc44a2bd44
jeonbinggu <tinyjbk123@gmail.com>
2026-01-29 06:45:10 +0900

    refactor: 소셜 페이지 실제 데이터로 변경(#98)

--- src/pages/mypage/edit-info/SocialPage.tsx
+++ src/pages/mypage/edit-info/SocialPage.tsx
@@ -1,14 +1,18 @@
 import { KakaoFillIcon } from "@/assets/icon";
-import { info } from "@/constants/mypage/info.constant";
+import { useMe } from "@/hooks/member";
 
 export function SocialPage() {
+  const { data: meData } = useMe();
+
+  const social = meData?.roleData.user?.socialAccounts?.[0];
+
   return (
     <div className="py-6">
       <main className="rounded-md border border-neutral-800 p-5 text-neutral-100">
-        <p>{info.roleData.user?.socialAccounts.email}</p>
+        <p>{social?.email}</p>
         <div className="mt-3 flex gap-2">
           <KakaoFillIcon className="h-6 w-6 fill-orange-500" />
-          <p>카카오톡</p>
+          <p>{social?.provider}</p>
         </div>
       </main>
     </div>
