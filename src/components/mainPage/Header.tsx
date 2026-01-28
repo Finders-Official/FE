98c394b014d5b12e1e973e51aa8c3aedfea27527
hyunseo-han <ols3040015@naver.com>
2026-01-28 22:49:40 +0900

    chore: 헤더 패딩 수정(#102)

--- src/components/mainPage/Header.tsx
+++ src/components/mainPage/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 flex h-14 w-full items-center justify-between bg-neutral-900 px-5">
+    <header className="sticky top-0 z-50 flex h-14 w-full items-center justify-between bg-neutral-900">
       <div>
         <Link
           to="/"
