b9a9a206654e6316513e53ccf16ffa411d499dd1
MinSeokKim <kimjunseok23@gmail.com>
2026-01-29 22:24:10 +0900

    feat: PhotoLabDetail에 캐시 컨트롤 추가(#108)

--- src/hooks/photoLab/usePhotoLabDetail.ts
+++ src/hooks/photoLab/usePhotoLabDetail.ts
@@ -15,5 +15,7 @@ export function usePhotoLabDetail(
     queryFn: () => getPhotoLabDetail(photoLabId!, params),
     select: (res) => res.data,
     enabled: !!photoLabId,
+    staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 15,
   });
 }
