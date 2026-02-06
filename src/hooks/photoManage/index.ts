78a1992d92aedba2102345261644f3c494fe5849
MinSeokKim <kimjunseok23@gmail.com>
2026-02-06 22:50:29 +0900

    feat: 스캔 사진 관련 hook추가(#117)

--- src/hooks/photoManage/index.ts
+++ src/hooks/photoManage/index.ts
@@ -1,5 +1,6 @@
 export { usePrintOptions } from "./usePrintOptions";
 export { usePrintQuote } from "./usePrintQuote";
 export { useCreatePrintOrder } from "./useCreatePrintOrder";
+export { useScanResults } from "./useScanResults";
 export { usePhotoLabAccount } from "./usePhotoLabAccount";
 export { useConfirmDepositReceipt } from "./useConfirmDepositReceipt";
