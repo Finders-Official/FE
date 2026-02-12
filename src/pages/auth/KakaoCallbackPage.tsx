a02ec616f9dc3a655661db4e0d982d8877ccce35
jeonbinggu <tinyjbk123@gmail.com>
2026-02-12 13:56:09 +0900

    refactor: 취소 버튼 누를시 로그인 페이지로 리다이렉 처리(#238)

--- src/pages/auth/KakaoCallbackPage.tsx
+++ src/pages/auth/KakaoCallbackPage.tsx
@@ -1,8 +1,32 @@
+import { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router";
 import { useKakaoOauth } from "@/hooks/auth/login";
-import { useNavigate } from "react-router";
 
 export function KakaoCallbackPage() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  //1 카카오 취소/거부 처리
+  useEffect(() => {
+    const sp = new URLSearchParams(location.search);
+    const error = sp.get("error");
+    const errorDesc = sp.get("error_description");
+
+    console.log(errorDesc);
+
+    // 취소/거부/실패 케이스는 여기서 전부 로그인으로 보냄
+    if (error) {
+      navigate("/auth/login", { replace: true });
+      return;
+    }
+
+    // code 자체가 없으면 비정상 접근/실패로 간주
+    const code = sp.get("code");
+    if (!code) {
+      navigate("/auth/login", { replace: true });
+      return;
+    }
+  }, [location.search, navigate]);
 
   //1. 기존 회원 : 바로 메인페이지로 리다이렉
   //2. 신규 회원: 온보딩 화면으로 리다이렉
