e637da735ade9b47ed629f224f8a4fa335bf656d
jeonbinggu <tinyjbk123@gmail.com>
2026-05-11 17:21:25 +0900

    design: 현상소 카드 디자인 변경(#271)

--- src/components/mypage/PhotoLabCard.tsx
+++ src/components/mypage/PhotoLabCard.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BriefcaseIcon, ClockIcon, StarIcon } from "@/assets/icon";
+import { StarIcon } from "@/assets/icon";
 import type { PhotoLab } from "@/types/mypage/photolab";
 import { Link } from "react-router";
 
@@ -24,13 +24,13 @@ export const PhotoLabCard = ({ photoLab, onToggleLike }: Props) => {
     : "fill-none text-white";
 
   return (
-    <div className="border-neutral-875 mt-2 border-b p-4">
+    <div className="mt-2 border-b border-neutral-800 py-4">
       <div className="group relative">
         {/*즐겨찾기 버튼: 클릭 시 링크 이동 방지 + UI 즉시 토글 */}
         <button
           type="button"
           aria-label={isFavorite ? "즐겨찾기 해제" : "즐겨찾기 등록"}
-          className="absolute top-1 right-1 z-10 inline-flex h-10 w-10 items-center justify-center rounded-full"
+          className="absolute top-3 right-1 z-10 flex inline-flex h-10 w-10 flex-col items-center justify-center rounded-full"
           onClick={(e) => {
             e.preventDefault();
             e.stopPropagation();
@@ -43,6 +43,7 @@ export const PhotoLabCard = ({ photoLab, onToggleLike }: Props) => {
           }}
         >
           <StarIcon className={`h-6 w-6 ${starColorClass}`} />
+          {/* <p className="text-[0.725rem] text-neutral-400">{photoLab.totalFavorites}</p> */}
         </button>
 
         <Link
@@ -50,44 +51,26 @@ export const PhotoLabCard = ({ photoLab, onToggleLike }: Props) => {
           className="block rounded-2xl"
           aria-label={`${photoLab.name} 상세로 이동`}
         >
-          <h2 className="text-[1.25rem] font-semibold tracking-[-0.0225rem] text-neutral-100">
-            {photoLab.name}
-          </h2>
+          {/* 세부 내용 */}
+          <div className="flex gap-3">
+            {/* 이미지 */}
+            <img
+              src={photoLab.imageUrls[0]}
+              alt={photoLab.name}
+              className="h-[3.75rem] w-[3.75rem] rounded-md"
+            />
+            <section className="flex flex-col p-1">
+              <h2 className="text-[1.175rem] font-semibold tracking-[-0.0225rem] text-neutral-100">
+                {photoLab.name}
+              </h2>
 
-          {/* 태그 */}
-          <section className="mt-3 flex flex-wrap gap-2">
-            {photoLab.tags.slice(0, 3).map((tag, idx) => (
-              <span
-                key={`${tag}-${idx}`}
-                className="inline-flex items-center rounded-md bg-[rgba(233,78,22,0.40)] p-1 text-[0.725rem] text-orange-200"
-              >
-                {tag}
-              </span>
-            ))}
-          </section>
-
-          {/* 주소 + 거리 */}
-          <section className="mt-1 flex items-center gap-1 text-[0.85rem] font-light text-neutral-200">
-            <span>{photoLab.address}</span>
-            <span>({photoLab.distanceText}km)</span>
-          </section>
-
-          {/* 하단 지표 */}
-          <section className="mt-2 flex items-center gap-3 text-[0.85rem] font-light text-neutral-200">
-            <div className="flex items-center gap-2">
-              <BriefcaseIcon className="h-3 w-3" />
-              <span>
-                총 작업 건 수 <span>{photoLab.totalWorkCount}건</span>
-              </span>
-            </div>
-
-            <div className="flex items-center gap-2">
-              <ClockIcon className="h-3 w-3" />
-              <span>
-                작업 소요 시간 <span>{photoLab.estimatedMinutes}분</span>
-              </span>
-            </div>
-          </section>
+              {/* 주소 + 거리 */}
+              <section className="mt-1 flex items-center gap-1 text-[0.85rem] font-light text-neutral-200">
+                <span>{photoLab.address}</span>
+                <span>({photoLab.distanceText}km)</span>
+              </section>
+            </section>
+          </div>
         </Link>
       </div>
     </div>
