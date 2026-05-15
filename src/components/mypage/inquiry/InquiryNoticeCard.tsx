ebd84aa8af8c607bfa4a5fcd8e672356beb3034f
jeonbinggu <tinyjbk123@gmail.com>
2026-05-16 00:31:40 +0900

    refactor: 컴포넌트 내부 구조 변경(#273)

--- src/components/mypage/inquiry/InquiryNoticeCard.tsx
+++ src/components/mypage/inquiry/InquiryNoticeCard.tsx
@@ -2,16 +2,25 @@ export function InquiryNoticeCard() {
   return (
     <div>
       <ul className="flex flex-col gap-1">
-        <li className="text-indent-[-1rem] list-inside list-disc text-[0.75rem] text-neutral-500">
-          문의 답변은 영업일 기준 익일 내로 완료됩니다. (주말 및 공휴일 제외)
+        <li className="flex gap-2 text-[0.75rem] leading-[155%] font-normal tracking-[-0.01625rem] text-neutral-400">
+          <span className="mt-1.5 h-1 w-1 rounded-full bg-neutral-400" />
+          <span className="flex-1">
+            문의 답변은 영업일 기준 익일 내로 완료됩니다.(주말 및 공휴일 제외)
+          </span>
         </li>
-        <li className="text-indent-[-1rem] list-inside list-disc text-[0.75rem] text-neutral-500">
-          1:1 문의 접수 내역은 관련 규정에 따라 회원탈퇴일로부터 3년간 안전하게
-          보관됩니다.
+        <li className="flex gap-2 text-[0.75rem] leading-[155%] font-normal tracking-[-0.01625rem] text-neutral-400">
+          <span className="mt-1.5 h-1 w-1 rounded-full bg-neutral-400" />
+          <span className="flex-1">
+            1:1 문의 접수 내역은 관련 규정에 따라 회원탈퇴일로부터 3년간
+            안전하게 보관됩니다.
+          </span>
         </li>
-        <li className="text-indent-[-1rem] list-inside list-disc text-[0.75rem] text-neutral-500">
-          욕설, 비방, 악의적인 목적의 문의는 답변이 제한되거나 서비스 이용에
-          불이익이 발생할 수 있습니다.
+        <li className="flex gap-2 text-[0.75rem] leading-[155%] font-normal tracking-[-0.01625rem] text-neutral-400">
+          <span className="mt-1.5 h-1 w-1 rounded-full bg-neutral-400" />
+          <span className="flex-1">
+            욕설, 비방, 악의적인 목적의 문의는 답변이 제한되거나 서비스 이용에
+            불이익이 발생할 수 있습니다.
+          </span>
         </li>
       </ul>
     </div>
