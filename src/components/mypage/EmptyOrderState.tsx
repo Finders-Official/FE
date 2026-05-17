f7eb3305ac24b35f14fb8388662a2cbc23bd0aca
jeonbinggu <tinyjbk123@gmail.com>
2026-05-17 21:56:33 +0900

    feat: 줄바꿈 처리 추가(#273)

--- src/components/mypage/EmptyOrderState.tsx
+++ src/components/mypage/EmptyOrderState.tsx
@@ -1,4 +1,5 @@
 import { BigLogoIcon } from "@/assets/icon";
+import React from "react";
 
 type EmptyOrderStateProps = {
   description: string;
@@ -9,8 +10,13 @@ export const EmptyOrderState = ({ description }: EmptyOrderStateProps) => {
     <div className="flex w-full flex-col items-center justify-center">
       <div className="flex flex-col items-center gap-4">
         <BigLogoIcon className="flex h-23.5 w-23.5 items-center justify-center rounded-full text-neutral-400" />
-        <h2 className="text-center text-[1rem] leading-[155%] font-normal tracking-[-0.02rem] text-neutral-300">
-          {description}
+        <h2 className="text-center text-[1rem] leading-[155%] font-normal tracking-[-0.02rem] whitespace-pre-wrap text-neutral-300">
+          {description.split(/\\n|\n/).map((line, index) => (
+            <React.Fragment key={index}>
+              {line}
+              <br />
+            </React.Fragment>
+          ))}
         </h2>
       </div>
     </div>
