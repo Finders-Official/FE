8932b09dd9fbd840b851b6b7a9ba27869dc8635d
jeonbinggu <tinyjbk123@gmail.com>
2026-05-25 21:58:32 +0900

    feat: 공지사항 조회, 상세 조회 api hook 구현(#279)

--- src/apis/my/index.ts
+++ src/apis/my/index.ts
@@ -1,3 +1,7 @@
 export { getLikedPosts } from "./likedPost.api";
 export { getMyPosts } from "./myPost.api";
 export { favoritePhotoLab } from "./likedPhotoLab";
+
+//notices
+export { getNoticeList } from "./notices/getNoticeList";
+export { getNoticeDetail } from "./notices/getNoticeList";
--- src/hooks/my/index.ts
+++ src/hooks/my/index.ts
@@ -1,3 +1,7 @@
 export { useLikedPostsInfinite } from "./useLikedPostsInfinite";
 export { useMyPostsInfinite } from "./useMyPostsInfinite";
 export { useLikedPhotoLabsInfinite } from "./useLikedPhotoLabsInfinite";
+
+//notices
+export { useGetNoticeList } from "./notices/useGetNoticeList";
+export { useNoticeDetail } from "./notices/useGetNoticeDetail";
