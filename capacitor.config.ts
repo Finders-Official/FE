446b3d5a86c17c1ec0b7763501d6bff8b74638a7
hyunseo-han <ols3040015@naver.com>
2026-05-27 15:30:59 +0900

    chore: Capacitor iOS 설정 파일 추가(#277)

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.finders.app",
+  appName: "Finders",
+  webDir: "dist",
+  appendUserAgent: "FindersApp",
+};
+
+export default config;
