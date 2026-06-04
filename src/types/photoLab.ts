0faa1076d1131e4cdb515deaed623503e3acc6fd
DESKTOP-4IEUVNT\rin42 <rin4219@naver.com>
2026-06-04 10:29:49 +0900

    chore: mock 데이터 삭제

--- src/types/photoLab.ts
+++ src/types/photoLab.ts
@@ -146,39 +146,6 @@ export type PhotoLab = {
   dist: string;
 };
 
-export const results: PhotoLab[] = [
-  {
-    id: "1",
-    name: "파인더스 상도점",
-    addr: "서울 동작구 상도 1동",
-    dist: "1.5km",
-  },
-  {
-    id: "2",
-    name: "파인더스 흑석점",
-    addr: "서울 동작구 상도 1동",
-    dist: "1.5km",
-  },
-  {
-    id: "3",
-    name: "파인더스 홍대점",
-    addr: "서울 동작구 상도 1동",
-    dist: "1.5km",
-  },
-  {
-    id: "4",
-    name: "파인더스 강남점",
-    addr: "서울 동작구 상도 1동",
-    dist: "1.5km",
-  },
-  {
-    id: "5",
-    name: "파인더스 건대점",
-    addr: "서울 동작구 상도 1동",
-    dist: "1.5km",
-  },
-];
-
 // 현상소 상세 페이지 타입 (PL-020)
 export interface PhotoLabLocation {
   latitude: number;
