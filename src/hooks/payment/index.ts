bb7d72cdeda7ff6e85607b79822dcd13ef2fdf17
MinSeokKim <kimjunseok23@gmail.com>
2026-06-11 21:54:35 +0900

    feat: Apple 인앱결제 구매 훅 추가(#287)

--- src/hooks/payment/index.ts
+++ src/hooks/payment/index.ts
@@ -1,6 +1,7 @@
 export { useVerifyGooglePayment } from "./useVerifyGooglePayment";
 export { useVerifyApplePayment } from "./useVerifyApplePayment";
 export { usePurchaseCredit, type PurchaseOutcome } from "./usePurchaseCredit";
+export { usePurchaseCreditApple } from "./usePurchaseCreditApple";
 export { useReconcileGooglePurchases } from "./useReconcileGooglePurchases";
 export { useCompletePortonePayment } from "./useCompletePortonePayment";
 export {
