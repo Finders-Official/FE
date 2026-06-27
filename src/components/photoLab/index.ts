903e657ab19f8385f548d1b6b5f7c66d5f9cb0cb
MinSeokKim <kimjunseok23@gmail.com>
2026-06-27 21:53:39 +0900

    refactor: v1 현상소 카드(LabCard) 컴포넌트 삭제(#298)

--- src/components/photoLab/index.ts
+++ src/components/photoLab/index.ts
@@ -1,8 +1,5 @@
-export { default as TagBadge } from "./TagBadge";
-export { default as LabCard } from "./LabCard";
 export { default as LabList } from "./LabList";
 export { default as FilterTagList } from "./FilterTagList";
 export { default as TimeSlotList } from "./TimeSlotList";
 export { default as RegionSelector } from "./RegionSelector";
 export { default as FilterBottomSheet } from "./FilterBottomSheet";
-export { default as LabCardSkeleton } from "./LabCardSkeleton";
