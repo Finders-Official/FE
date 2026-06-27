4f7840de55c219ef936b560b48f1e189475c42f9
MinSeokKim <kimjunseok23@gmail.com>
2026-06-28 00:29:06 +0900

    refactor: UI 오류 수정 및 주석 정리(#298)

--- src/hooks/my/useLikedPhotoLabsInfinite.ts
+++ src/hooks/my/useLikedPhotoLabsInfinite.ts
@@ -1,6 +1,10 @@
 import { favoritePhotoLab } from "@/apis/my";
 import type { GetFavoritePhotoLabsResponse } from "@/types/mypage/photolab";
-import { useInfiniteQuery, type InfiniteData } from "@tanstack/react-query";
+import {
+  useInfiniteQuery,
+  keepPreviousData,
+  type InfiniteData,
+} from "@tanstack/react-query";
 
 export const FAVORITE_PHOTO_LABS_QUERY_KEY = [
   "photoLabs",
@@ -28,5 +32,7 @@ export function useLikedPhotoLabsInfinite(
       if (isLast) return undefined;
       return allPages.length; // page가 0부터 시작 -> 다음은 pages.length
     },
+    // 위치 정보가 뒤늦게 들어와 queryKey가 바뀔 때 스켈레톤 재깜빡임 방지
+    placeholderData: keepPreviousData,
   });
 }
