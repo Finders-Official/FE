8737cdbf9389f168378221d12c989a6301e2ac7f
MinSeok1_2 <kimjunseok23@gmail.com>
2026-07-01 23:13:06 +0900

    refactor: 현상소 검색 결과 스켈레톤을 컴포넌트로 추출(#302)

--- src/pages/photoFeed/FindPhotoLabPage.tsx
+++ src/pages/photoFeed/FindPhotoLabPage.tsx
@@ -3,6 +3,7 @@ import { CTA_Button } from "@/components/common/CTA_Button";
 import { Checkbox } from "@/components/common/CheckBox";
 import { useRef, useState } from "react";
 import { HighlightText } from "@/components/photoFeed/upload/highlightText";
+import LabSearchResultSkeleton from "@/components/photoFeed/upload/LabSearchResultSkeleton";
 import type { LabSearchResponse } from "@/types/photoFeed/labSearch";
 import { useNewPostState } from "@/store/useNewPostState.store";
 import { useNavigate } from "react-router";
@@ -173,18 +174,7 @@ export default function FindPhotoLabPage() {
   /** 검색 결과 리스트 */
   const renderSearchList = () => {
     if (isLoading) {
-      return (
-        <ul className="flex flex-col gap-4 p-4">
-          {Array.from({ length: 8 }).map((_, i) => (
-            <li key={i} className="list-none">
-              <div className="t-skel-sheen flex flex-col gap-2 py-4">
-                <div className="h-5 w-52 rounded-md bg-neutral-800/60" />
-                <div className="h-4 w-72 rounded-md bg-neutral-800/40" />
-              </div>
-            </li>
-          ))}
-        </ul>
-      );
+      return <LabSearchResultSkeleton />;
     }
     if (isError) {
       return (
