ae7e459933afd2c7c68d1cbce61c72b5dc9caa70
MinSeokKim <kimjunseok23@gmail.com>
2026-07-04 00:02:45 +0900

    feat: 닉네임/전화번호 수정 화면에 입력 오류 shake 피드백 적용(#302)

--- src/pages/mypage/edit-info/PhoneEditPage.tsx
+++ src/pages/mypage/edit-info/PhoneEditPage.tsx
@@ -101,6 +101,7 @@ export function PhoneEditPage() {
               }
               onChange={f.handleVerifiedNumberChange}
               disabled={f.lockPhoneForm}
+              shakeKey={f.phoneShakeKey}
             />
 
             <ActionButton
